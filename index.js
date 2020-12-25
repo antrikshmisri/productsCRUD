401bcb63aac1f3b7e9670772c4534f5d0cd99fc2
antrikshmisri <antrikshmisri@gmail.com>
2020-12-25 18:58:55 +0530

    feat: added create and read operations

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const Product = require('./models/product')
+
+app.set('views' , path.join(__dirname , 'views'))
+app.set('view engine' , 'ejs')
 mongoose.connect('mongodb://localhost:27017/productsdb' , {useNewUrlParser: true})
     .then(() => {
         console.log('Mongo connection successful')
@@ -11,3 +14,17 @@ mongoose.connect('mongodb://localhost:27017/productsdb' , {useNewUrlParser: true
         console.log(`Mongo Connection failed, Error:- ${err}`);
     })
 
+app.get('/products' , async (req , res) =>{
+    const products = await Product.find({})
+    res.render('products/index' , {products})
+})
+
+app.get('/products/:id' , async (req , res)=>{
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    res.render('products/show' , {product})
+})
+
+app.listen(5050 , ()=>{
+    console.log("Listening on port 5050")
+})
